f2a4ef251241ba4f2f0ae142120966fc2bbecd0a
yurimellie <unloveparty@naver.com>
2018-02-13 17:49:41 +0900

    [유림]포트폴리오다오

---
+++ src/Soin/jspweb/dao/PortfolioDao.java
@@ -0,0 +1,22 @@
+package Soin.jspweb.dao;
+
+import java.util.List;
+
+import Soin.jspweb.entity.Portfolio;
+
+public interface PortfolioDao {
+
+	List<Portfolio> getList();
+	List<Portfolio> getList(String query);
+	List<Portfolio> getList(int page);
+	List<Portfolio> getList(int page, String array, String category, String query);
+
+	
+	
+	Portfolio get(String id);
+	//Portfolio getPrev(String id);
+	//Portfolio getNext(String id);
+	
+	
+	//List<Portfolio> getList(String cityCategory, String townCategory);
+}
