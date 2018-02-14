24bab051fed72ff63584847e589d3a66a7f3d9f4
phhaanz <phhaanz@users.noreply.github.com>
2018-02-14 14:57:20 +0900

    2018-02-14 유노

--- src/Soin/jspweb/dao/InteriorTipDao.java
+++ src/Soin/jspweb/dao/InteriorTipDao.java
@@ -2,7 +2,14 @@ package Soin.jspweb.dao;
 
 import java.util.List;
 
+import Soin.jspweb.entity.InteriorTip;
+
 public interface InteriorTipDao {
 	
 	List<InteriorTip> getList();
+	List<InteriorTip> getList(int page);
+	List<InteriorTip> getList(String query);
+	List<InteriorTip> getList(int page, String query, String Category, String Material);
+	
+	InteriorTip get(String id);
 }
