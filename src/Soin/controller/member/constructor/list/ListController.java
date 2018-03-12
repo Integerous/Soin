5c67aa240626e7082de3bd62f12bda82cb4d68f2
user <user@DESKTOP-PNJ5TCE>
2018-03-12 16:42:51 +0900

    우진_controller

--- src/Soin/controller/member/constructor/list/ListController.java
+++ src/Soin/controller/member/constructor/list/ListController.java
@@ -64,14 +64,6 @@ public class ListController extends HttpServlet{
 		TilesContainer container = TilesAccess.getContainer(applicationContext);
 		ServletRequest servletRequest = new ServletRequest(applicationContext, request, response);
 		container.render("Member.Constructor.List.list8", servletRequest);
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'master' of https://github.com/ryan-js/SoinProject.git
->>>>>>> branch 'master' of https://github.com/ryan-js/SoinProject.git
 	}
-
 }
